796d976af5fc01f3f4a4884f28ba8cad5daf162f
Lucas <ticet>
2026-03-24 17:40:20 +0100

    dodano

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS1
+{
+    internal class Class1
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
